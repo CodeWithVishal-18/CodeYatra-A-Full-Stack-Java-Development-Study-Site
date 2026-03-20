9f0687c347dc256d1ecac772a87c3c8cf76ab85e
Vishal Shukla <vishal.shuklaji45@gmail.com>
2026-03-21 00:56:23 +0530

    Change the Application name and also add animation and color to Navbar logo icon

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg bg-secondary-subtle">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to={"/"}>CodeYatra</Link>
+                    <Link className="navbar-brand" to={"/"}><span className='codeColorLogo'>Code</span><span className='saarthiColorLogo'>Saarthi</span></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
